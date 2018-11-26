c808f403914327fc16dc39d616af2fc23f53eaf2
orange4glace <orange4glace@gmail.com>
2018-11-26 19:51:39 +0900

    clean

---
+++ src/decoder/frame_base.h
@@ -0,0 +1,15 @@
+#ifndef OLIVE_FRAME_BASE_H
+#define OLIVE_FRAME_BASE_H
+
+namespace olive {
+
+template <typename T>
+struct FrameBase : struct Frame {
+
+
+
+};
+
+}
+
+#endif // OLIVE_FRAME_BASE_H
